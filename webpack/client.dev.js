2a0b174396e4581f62711f8f2f04d803f1552046
Steve Akinyemi <appcypher@outlook.com>
2019-03-09 22:35:41 +0100

    Update client.dev.js

--- webpack/client.dev.js
+++ webpack/client.dev.js
@@ -2,65 +2,70 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   context: path.join(__dirname, '../'),
   devtool: 'source-map',
   entry: [
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.js'
+    './src/index.js',
   ],
   output: {
-    path: path.join(__dirname, '../public'),
-    filename: './bundle.min.js',
-    publicPath: '/',
+    filename: 'bundle.min.js',
+    publicPath: '/public', // directory where dev-server looks at for bundle
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '../'),
+    compress: true,
+    port: 9000,
   },
   module: {
     rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['react', 'env', 'stage-1'],
-          },
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env', 'stage-1'],
         },
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
-      },
-      {
-        test: /\.(ttf|eot|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {}
-          }
-        ]
-      },
-      {
-        test: /\.(svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'svg-url-loader',
-            options: {}
-          }
-        ]
-      },
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    },
+    {
+      test: /\.(png|jpg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {},
+        },
+      ],
+    },
+    {
+      test: /\.(ttf|eot|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {},
+        },
+      ],
+    },
+    {
+      test: /\.(svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'svg-url-loader',
+          options: {},
+        },
+      ],
+    },
     ],
   },
   plugins: [],
