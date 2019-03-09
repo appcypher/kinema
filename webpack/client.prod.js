e2fb9905f15bc4f9628f85bd1e6e69abcb234f91
Steve Akinyemi <appcypher@outlook.com>
2019-03-09 22:36:19 +0100

    Update client.prod.js

--- webpack/client.prod.js
+++ webpack/client.prod.js
@@ -3,67 +3,67 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   context: path.join(__dirname, '../'),
   devtool: 'source-map',
   entry: [
-    './src/index.js'
+    './src/index.js',
   ],
   output: {
-    path: path.join(__dirname, '../public'),
+    path: path.join(__dirname, '../public'), // directory where bundles are built
     filename: './bundle.min.js',
-    publicPath: '/',
   },
   module: {
     rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['react', 'env', 'stage-1'],
-          },
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env', 'stage-1'],
         },
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
-      },
-      {
-        test: /\.(ttf|eot|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {}
-          }
-        ]
-      },
-      {
-        test: /\.(svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'svg-url-loader',
-            options: {}
-          }
-        ]
-      },
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    },
+    {
+      test: /\.(png|jpg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {},
+        },
+      ],
+    },
+    {
+      test: /\.(ttf|eot|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {},
+        },
+      ],
+    },
+    {
+      test: /\.(svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'svg-url-loader',
+          options: {},
+        },
+      ],
+    },
     ],
   },
   plugins: [
     new UglifyJSPlugin({
-      sourceMap: true
+      sourceMap: true,
     }),
   ],
 };
